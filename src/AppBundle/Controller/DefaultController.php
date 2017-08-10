cf43075943e868bcb9ea62e1fa235f9f71c2dbac
Olivier Revollat <revollat@gmail.com>
2017-08-10 14:14:40 +0200

    ajout fixtures users

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use Knp\Component\Pager\Paginator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +18,19 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/listing/{page}", name="listing")
+     */
+    public function listingAction(Paginator $paginator, ObjectManager $em, $page = 1)
+    {
+        $query = $em->createQuery('SELECT q FROM AppBundle:Question q ORDER BY q.intitule ASC');
+        $pagination = $paginator->paginate($query,$page);
+
+        return $this->render('default/listing.html.twig', [
+            'pagination' => $pagination
         ]);
     }
 }
---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,43 @@
+<?php
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $user1 = new User();
+        $user1->setUsername('oliv');
+        $user1->setPlainPassword('pass');
+        $user1->setEmail('oliv@exemple.org');
+        $user1->setEnabled(true);
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setUsername('toto');
+        $user2->setPlainPassword('toto');
+        $user2->setEmail('toto@exemple.org');
+        $user2->setEnabled(true);
+        $manager->persist($user2);
+
+        $adm = new User();
+        $adm->setUsername('adm');
+        $adm->setPlainPassword('mdp4adm');
+        $adm->setEmail('adm@exemple.org');
+        $adm->setEnabled(true);
+        $adm->setRoles(array('ROLE_ADMIN'));
+        $manager->persist($adm);
+
+        $manager->flush();
+    }
+    
+    public function getOrder()
+    {
+        return 5;
+    }
+    
+}
